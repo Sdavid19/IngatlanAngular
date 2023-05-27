034f71e42fb516877f4575cdff4d695a6c1b93ee
SulyokDavid <sulyokdavid03@gmail.com>
2023-05-27 18:46:10 +0200

    service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { NModel } from '../models/n.model';
+import { OneModel } from '../models/1.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  apiUrl = "http://127.0.0.1:8000/api"
+
+  constructor(private http: HttpClient) { }
+
+  getAllNSide(): Observable<NModel[]>{
+    return this.http.get<NModel[]>(`${this.apiUrl}/NSide`);
+  }
+
+  getAllOneSide(): Observable<OneModel[]>{
+    return this.http.get<OneModel[]>(`${this.apiUrl}/OneSide`);
+  }
+
+  getNSdeByNumber(num: number | string): Observable<NModel>{
+    return this.http.get<NModel>(`${this.apiUrl}/NSide/${num}`);
+  }
 }
